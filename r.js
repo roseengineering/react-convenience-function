69f078faf0f9e6879de14edad7a3a95344f55712
george <george>
2016-02-19 19:02:39 -0500

    added umd prelude to r.js

--- r.js
+++ r.js
@@ -1,35 +1,43 @@
 
-function r(selector, attrs){
-    attrs = attrs == null ? {} : attrs;
-    var children,
-        tag = selector;
-
-    if (typeof attrs === 'object' && !attrs.props && !Array.isArray(attrs)){
-        children = [].slice.call(arguments, 2);
+(function (global, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory(require('react'));
     } else {
-        children = [].slice.call(arguments, 1);
-        attrs = {};
+        global.r = factory(global.React);
     }
+}(this, function (React) {
 
-    if (typeof selector === 'string'){
-        tag = 'div';
+    return function(selector, attrs){
+        attrs = attrs == null ? {} : attrs;
+        var children,
+            tag = selector;
 
-        var parser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[.+?\])/g, 
-            attrParser = /\[(.+?)(?:=("|'|)(.*?)\2)?\]/,
-            match, classes = [];
-
-        while ((match = parser.exec(selector)) != null){
-            if (match[1] === '' && match[2]) tag = match[2];
-            else if (match[1] === '#') attrs.id = match[2];
-            else if (match[1] === '.') classes.push(match[2]);
-            else if (match[3][0] === '[') {
-                var pair = attrParser.exec(match[3]);
-                attrs[pair[1]] = pair[3] || (pair[2] ? '' :true);
-            }
+        if (typeof attrs === 'object' && !attrs.props && !Array.isArray(attrs)){
+            children = [].slice.call(arguments, 2);
+        } else {
+            children = [].slice.call(arguments, 1);
+            attrs = {};
         }
-        if (classes.length) attrs['className'] = classes.join(' ');
+
+        if (typeof selector === 'string'){
+            tag = 'div';
+
+            var parser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[.+?\])/g, 
+                attrParser = /\[(.+?)(?:=("|'|)(.*?)\2)?\]/,
+                match, classes = [];
+
+            while ((match = parser.exec(selector)) != null){
+                if (match[1] === '' && match[2]) tag = match[2];
+                else if (match[1] === '#') attrs.id = match[2];
+                else if (match[1] === '.') classes.push(match[2]);
+                else if (match[3][0] === '[') {
+                    var pair = attrParser.exec(match[3]);
+                    attrs[pair[1]] = pair[3] || (pair[2] ? '' :true);
+                }
+            }
+            if (classes.length) attrs['className'] = classes.join(' ');
+        }
+
+        return React.createElement.apply(React, [].concat(tag, attrs, children));
     }
-
-    return React.createElement.apply(React, [].concat(tag, attrs, children));
-};
-
+}));
